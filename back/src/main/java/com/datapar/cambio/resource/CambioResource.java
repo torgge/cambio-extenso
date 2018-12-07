93b895f0a713aa94a2ba5d1b6a1d338d13c41dbb
George A. A. Bonespírito <george.bonespirito@datapar.com.py>
2018-12-07 13:39:24 -0300

    Liberado acesso para todos os hosts.

--- back/src/main/java/com/datapar/cambio/resource/CambioResource.java
+++ back/src/main/java/com/datapar/cambio/resource/CambioResource.java
@@ -4,6 +4,7 @@ package com.datapar.cambio.resource;
 import com.datapar.cambio.service.CambioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class CambioResource {
     @Autowired
     CambioService service;
     //http://ip:port/extenso/v1/guarani/2800000
+    @CrossOrigin(origins = { "*", "http://localhost:9797" }, maxAge = 6000)
     @RequestMapping("/guarani/{vlr}")
     public ResponseEntity<String> getCambioGuarani(@PathVariable("vlr") Double vlr) {
         String extenso = service.getExtensoGuarani(vlr);
@@ -22,6 +24,7 @@ public class CambioResource {
     }
 
     //http://ip:port/extenso/v1/real/2800000
+    @CrossOrigin(origins = { "*", "http://localhost:9797" }, maxAge = 6000)
     @RequestMapping("/real/{vlr}")
     public ResponseEntity<String> getCambioReal(@PathVariable("vlr") Double vlr) {
         String extenso = service.getExtensoReal(vlr);
